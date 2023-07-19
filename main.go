7e3164eba48e68aa6977ba6ec1bf84977b435c2d
jboothomas <44343760+jboothomas@users.noreply.github.com>
2023-07-19 11:39:12 +0200

    Add files via upload

---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2023 Jean-Baptiste Thomas <jboothomas@gmail.com>
+This file is part of CLI application p53.
+*/
+package main
+
+import "pS3/cmd"
+
+func main() {
+	cmd.Execute()
+}
